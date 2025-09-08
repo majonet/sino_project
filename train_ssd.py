4cff77348b154874fcab926318a65e3e0a5eebad
majonet <salamspacex@gmail.com>
2025-09-08 11:57:03 +0330

    train_ssd.py

--- train_ssd.py
+++ train_ssd.py
@@ -147,6 +147,8 @@ def train(loader, net, criterion, optimizer, device, debug_steps=100, epoch=-1):
 
 
 def test(loader, net, criterion, device):
+    print("Number of samples in dataset:", len(loader.dataset))
+    print("Number of batches per epoch:", len(loader))
     net.eval()
     running_loss = 0.0
     running_regression_loss = 0.0
