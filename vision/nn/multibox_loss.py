7d1cba068fa202524bea6ea2de9081e36a96b3bd
majonet <salamspacex@gmail.com>
2025-09-13 16:03:06 +0330

    multibox_loss.py

--- vision/nn/multibox_loss.py
+++ vision/nn/multibox_loss.py
@@ -38,6 +38,10 @@ class MultiboxLoss(nn.Module):
             mask = box_utils.hard_negative_mining(loss, labels, self.neg_pos_ratio)
 
         confidence = confidence[mask, :]
+        print(30*"---")
+        print("confidence : ",confidence.reshape(-1, num_classes))
+        print("labels : ",    labels[mask]  )
+        print(30*"---")
         classification_loss = F.cross_entropy(confidence.reshape(-1, num_classes), labels[mask], size_average=False)
         pos_mask = labels > 0
         predicted_locations = predicted_locations[pos_mask, :].reshape(-1, 4)
